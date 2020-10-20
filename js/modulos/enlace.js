9581020850f7f67372b1f673f2be0bdd177a5ec4
jorgeGit8791 <andresmontoya8791@gmail.com>
2020-10-19 23:37:46 -0300

    estilos response

--- js/modulos/enlace.js
+++ js/modulos/enlace.js
@@ -8,15 +8,12 @@ export default function sudoku(empezarDidicultad) {
             container = d.querySelector(".content-grid");
 
         let tammaño = null;
-        console.log(form.tamaño.value);
         
         if(e.target === form){
             e.preventDefault();
             if(container) container.innerHTML = "";
             (form.tamaño.value === "smoll")? tammaño = 9 : tammaño = 12;
-            console.log(tammaño)
             for (let i = 0; i < tammaño*tammaño; i++) {
-                console.log("esta en el bucle " + tammaño )
                 const div = d.createElement("div");
                 div.classList.add("grids");
                 div.style.backgroundColor = "white";   
@@ -24,7 +21,6 @@ export default function sudoku(empezarDidicultad) {
                 div.textContent = `${i + 1}`;            
                 fragmento.insertBefore(div, fragmento.childNodes[fragmento.childNodes.length]);
             }
-            // console.log(fragmento.textContent)
             container.appendChild(fragmento);
             container.style.gridTemplateRows = `repeat(${tammaño}, 1fr)`;
             container.style.gridTemplateColumns = `repeat(${tammaño}, 1fr)`;
