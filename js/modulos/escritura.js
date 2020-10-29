5d0228b1f658ce593d9dd0ba8267b7cb79c51aab
jorgeGit8791 <andresmontoya8791@gmail.com>
2020-10-28 23:44:34 -0300

    correccion del commit anterior

--- js/modulos/escritura.js
+++ js/modulos/escritura.js
@@ -2,16 +2,16 @@ const d = document;
 
 export default function escrituraMovimiento(e,input,caja) {
     
-    const inputs = d.querySelectorAll(input),
-        cajaGrids = d.querySelector(caja);
+    // const inputs = d.querySelectorAll(input),
+    //     cajaGrids = d.querySelector(caja);
     
-    //  console.log(e.key)
-    // if(e.target.matches(inputs)){
-    //     inputs.forEach(el => {
-    //         if(!Number(e.key)) {
-    //             el.textContent = "";
-    //         }
-    //     })
-    }  
+    //  console.log(e.char)
+    // if(e.target.matches(input)){
+          
+    //     if(!Number(e.key)) {
+            
+    //     }
+        
+    // }  
 
 }
