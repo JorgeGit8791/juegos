64b88a3f555d8b09d8301c2bdc92f5aeee0dc4fe
jorgeGit8791 <andresmontoya8791@gmail.com>
2020-11-09 00:18:06 -0300

    interaccion js Time

--- js/modulos/time.js
+++ js/modulos/time.js
@@ -1,3 +1,4 @@
+"use strict"
 const d = document;
 
 export default function time(empezar,nuevoSudo,time,container) {
@@ -5,27 +6,46 @@ export default function time(empezar,nuevoSudo,time,container) {
         newPlay = d.getElementById(nuevoSudo),
         times = d.querySelector(time),
         containers = d.querySelector(container);
-        
-    d.addEventListener("click", e => {
-        const inputContent = containers.querySelectorAll("input")
-        let tammaño = (play.tamaño.value === "smoll")?  9 :  12;
-        console.log(inputContent.length);
-        if(inputContent.length/tammaño !== tammaño) return;
-        if(e.target === newPlay) {
-            times.innerHTML = "si";
+    let tiempo,
+        tiempoPlay,
+        contenido;
+    
+    const intervalTime = () => {
+            const inter = setInterval(() => {
+                tiempo = new Date();
+                contenido = `Empesaste ${tiempoPlay.getHours()}:${tiempoPlay.getMinutes()}:${tiempoPlay.getSeconds()} ---
+                Llevas ${tiempo.getHours()}:${tiempo.getMinutes()}:${tiempo.getSeconds()}`;
+                times.innerHTML = `${contenido}`;
+                },1000);
+                return;
         }
-    });
-        
-        
-        
+
+    
+            
+        d.addEventListener("click", e => {
+            tiempoPlay = new Date;
+            const inputContent = containers.querySelectorAll("input")
+            let tammaño = (play.tamaño.value === "smoll")?  9 :  12;
+            // console.log(inputContent.length);
+            if(inputContent.length/tammaño !== tammaño) return;
+            if(e.target === newPlay) {
+                intervalTime();
+            }
+        });
+    
+    
+    
     d.addEventListener("submit", e => {
+        tiempoPlay = new Date;
         const inputContent = containers.querySelectorAll("input")
         let tammaño = (play.tamaño.value === "smoll")?  9 :  12;
-        console.log(inputContent.length);
-
+        // console.log(inputContent.length);
+        
         if(inputContent.length/tammaño !== tammaño) return;
         if(e.target === play) {
-            times.innerHTML = "si si";
+
+            intervalTime()
+            
         }
     });
 
